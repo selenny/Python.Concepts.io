60ff5dd35e2e1009a579e50584348f7d8ee74e17
Terri Hall <webdev.terrihall@gmail.com>
2017-10-09 21:29:30 -0400

    Add files via upload

---
+++ THall.36.68.py
@@ -0,0 +1,57 @@
+# Python: 2.7.14
+#
+# Author: Terri Hall
+#
+# Purpose: Complete assignment and show basic knowledge each number in assignment.
+
+
+
+# 1. Assign an integer to a variable
+    # counter = 100
+# 2. Assign a string to a variable
+    # article = 'magazines'
+# 3. Assign a float to a variable
+    # myFloat = float(7)
+# 4. Use the print fuction and .format() notation to print out the variable you assigned.
+    # myFloat = float(70).format()
+    # print myFloat
+# 5. Use each of these operators +,-,*/+=,=,%
+    ''' myvariable = 44+27-1*32/1
+        correctBill = correctBill +=20
+        newRaise = 4%   '''
+# 6. Use of logical operators: and, or, not
+    ''' if (age > 18 AND age <=35)
+      if (age > 35 OR age < 60)
+      if age = True then NOT (age)  '''
+# 7. Use of conditional statements: if, elif, else
+    ''' var = 100
+      if var == 200:
+          print var
+      elif var == 300:
+          print var
+      else:
+          print "Ok This is the answer your looking for"   '''
+# 8. ''' count = 0
+         while (count < 9):
+             print 'The count is:', count
+             count = count + 1
+         print "Thank you"   '''
+# 9. Use of for loop
+    ''' for val in numbers:
+             sum = sum + val '''
+#10. Create a list and iterate through that list using a for loop to print each item out on a new line
+    ''' myList = ['20', '40', '60']
+        for elem in myList:
+            print elem '''
+# 11. Create a tuple and iterate through the list using a for loop
+    ''' animals = ['dog', 'cat', 'chicken', 'gorilla']
+        for animal in animals:
+            print("I have a" + animal + ".") '''
+#12. Define a function that returns a string variable
+    ''' def square(x):
+                y = X * X
+                return y
+        toSquare = 10
+        result = square(toSquare) '''
+#13. Call the function you defined above and print the result to the shell
+    # print "The result of" + str(toSquare) + "squared is" + str(result)
